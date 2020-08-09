f3ba9f8a088fd5c1963150b81106f15f898a8d09
Dmitriy Filtachev <o6jlako@mail.ru>
2020-08-09 08:01:00 +0300

    first result ready

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import * as tf from '@tensorflow/tfjs'
+import React from 'react';
+// import * as tf from '@tensorflow/tfjs'
 
 import { DisplayCanvas } from './components/DisplayCanvas';
 import './App.css';
 
 function App() {
-  const [tfModel, setTfModel] = useState<any>(null)
+  // const [tfModel, setTfModel] = useState<any>(null)
 
-  useEffect(() => {
-    if (tfModel === null) {
-      tf.loadLayersModel('file://../train/model/model.json').then(
-        model => model && setTfModel(model)
-      )
-    }
-  }, [tfModel])
+  // useEffect(() => {
+  //   if (tfModel === null) {
+  //     tf.loadLayersModel('file://../train/model/model.json').then(
+  //       model => model && setTfModel(model)
+  //     )
+  //   }
+  // }, [tfModel])
 
-  function prepareDataFromCanvas({ data }: ImageData) {
-    const values = Array.from(data).map((value: number, idx: number) =>
-      idx + (1 % 8) === 0 ? value / 255 : value,
-    );
-    tensorDetectionCNN(values)
-  }
+  // function prepareDataFromCanvas({ data }: ImageData) {
+  //   const values = Array.from(data).map((value: number, idx: number) =>
+  //     idx + (1 % 8) === 0 ? value / 255 : value,
+  //   );
+  //   tensorDetectionCNN(values)
+  // }
 
-  function tensorDetectionCNN(imgValues: number[]) {
-     // CNN with Tensorflow.js
-     const predictTensor = tfModel.predict(tf.tensor4d(imgValues, [1, 28, 28, 1])) as tf.Tensor;
-     const data = predictTensor.dataSync();
-     console.log(data)
+  // function tensorDetectionCNN(imgValues: number[]) {
+  //    // CNN with Tensorflow.js
+  //    const predictTensor = tfModel.predict(tf.tensor4d(imgValues, [1, 28, 28, 1])) as tf.Tensor;
+  //    const data = predictTensor.dataSync();
+  //    console.log(data)
+  // }
+
+  async function prepareDataFromCanvas({ data, width, height }: ImageData) {
+    const imageDataCopy = { data, width, height };
+    console.log(imageDataCopy)
+    const response = await fetch('http://localhost:8080/model', {
+      method: 'POST',
+      credentials: 'include',
+      referrerPolicy: 'no-referrer',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(imageDataCopy),
+    });
+    const result = await response.json();
+    console.log('client result', result);
   }
 
   return (
--- client/src/components/DisplayCanvas/DisplayCanvas.tsx
+++ client/src/components/DisplayCanvas/DisplayCanvas.tsx
@@ -1,14 +1,22 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './DisplayCanvas.css';
 
 type DisplayCanvasProps = {
-  prediction?: Function
-}
+  prediction: Function;
+};
+
+const DEFAULT_DRAWING_BOX = {
+  maxX: 0,
+  maxY: 0,
+  minX: Number.MAX_SAFE_INTEGER,
+  minY: Number.MAX_SAFE_INTEGER,
+};
 
 export function DisplayCanvas({ prediction }: DisplayCanvasProps) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
   const painting = useRef(false);
+  const [drawingBox, setDrawingBox] = useState(DEFAULT_DRAWING_BOX);
 
   useEffect(() => {
     ctxRef.current = canvasRef.current
@@ -37,15 +45,34 @@ export function DisplayCanvas({ prediction }: DisplayCanvasProps) {
   // }
 
   function draw({ nativeEvent }: React.MouseEvent) {
-    if (!painting.current || ctxRef.current === null) return;
+    if (
+      !painting.current ||
+      ctxRef.current === null ||
+      canvasRef.current === null
+    )
+      return;
     const { offsetX, offsetY } = nativeEvent;
 
-    ctxRef.current.lineWidth = 5;
+    ctxRef.current.lineWidth = 32;
     ctxRef.current.lineCap = 'round';
     ctxRef.current.lineTo(offsetX, offsetY);
     ctxRef.current.stroke();
     ctxRef.current.beginPath();
     ctxRef.current.moveTo(offsetX, offsetY);
+
+    const { left, top } = canvasRef.current.getBoundingClientRect();
+
+    const x = nativeEvent.clientX - left;
+    const y = nativeEvent.clientY - top;
+
+    setDrawingBox((box) => ({
+      maxX: Math.max(box.maxX, x),
+      maxY: Math.max(box.maxY, y),
+      minX: Math.min(box.minX, x),
+      minY: Math.min(box.minY, y),
+    }));
+
+    console.log(x, y, drawingBox)
   }
 
   function clearWindow() {
@@ -56,12 +83,49 @@ export function DisplayCanvas({ prediction }: DisplayCanvasProps) {
       canvasRef.current.width,
       canvasRef.current.height,
     );
+    setDrawingBox(DEFAULT_DRAWING_BOX);
   }
 
   function sendDataUrl(send: Function | undefined) {
-    if (send && ctxRef.current !== null) return send(
-      ctxRef.current.getImageData(0, 0, 28, 28)
-    )
+    if (!send || ctxRef.current === null || canvasRef.current === null) return;
+    const canvasCopy = document.createElement('canvas');
+    canvasCopy.width = 28;
+    canvasCopy.height = 28;
+    const copyContext = canvasCopy.getContext('2d');
+    const ratioX = canvasRef.current.width / 28;
+    const ratioY = canvasRef.current.height / 28;
+    console.log('ratio', ratioX, ratioY)
+    const drawBox = [
+      drawingBox.minX,
+      drawingBox.minY,
+      drawingBox.maxX,
+      drawingBox.maxY,
+    ];
+    console.log(drawBox)
+    const scaledSourceWidth = Math.min(
+      20,
+      Math.max(4, (drawBox[2] - drawBox[0] + 32) / ratioX),
+    );
+    const scaledSourceHeight = Math.min(
+      20,
+      (drawBox[3] - drawBox[1] + 32) / ratioY,
+    );
+    const dx = (28 - scaledSourceWidth) / 2;
+    const dy = (28 - scaledSourceHeight) / 2;
+
+    copyContext &&
+      copyContext.drawImage(
+        canvasRef.current,
+        drawBox[0] - 16,
+        drawBox[1] - 16,
+        drawBox[2] - drawBox[0] + 16,
+        drawBox[3] - drawBox[1] + 16,
+        dx,
+        dy,
+        scaledSourceWidth,
+        scaledSourceHeight,
+      );
+    return copyContext ? send(copyContext.getImageData(0, 0, 28, 28)) : null;
   }
 
   return (
